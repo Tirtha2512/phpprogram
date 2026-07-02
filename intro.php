447bb6136fa13faa16e06a18e735da21a0b66b03
Tirtha2512 <tirthagohel159@gmail.com>
2026-07-02 11:31:51 +0530

    Add files via upload

---
+++ intro.php
@@ -0,0 +1,24 @@
+<?php
+ $Name="Tirtha";
+ $Address="Marutichok rajkot-360005";
+ $City="Rjkot";
+ $Department="Computerscience";
+ $class="DS1";
+ $Div="E2";
+ $Uni_name="Atmiya_university";
+ echo "<table>";
+ echo "<tr>";
+echo $Name;
+echo "<br>";
+echo $Address;
+echo "<br>";
+echo $City;
+echo "<br>";
+echo $Department;
+echo "<br>";
+echo $class;
+echo "<br>";
+echo $Div;
+echo "<br>";
+echo $Uni_name;
+?>
