335b2dfa35b0c0a402366d24e91a96e01e8ede1c
Tirtha2512 <tirthagohel159@gmail.com>
2026-07-09 10:49:47 +0530

    Add files via upload

---
+++ string.php
@@ -0,0 +1,26 @@
+<?php
+echo chr(99);
+echo "<br>";
+
+
+echo ord("t");
+echo "<br>";
+
+echo strtoupper("tirtha");
+echo "<br>";
+
+echo strtolower("TIRTHA");
+echo "<br>";
+
+echo md5("tirtha");
+echo "<br>";
+
+$a=explode("t","this is testy food today");
+print_r($a);
+echo "<br>";
+
+/*$b=implode("t","this is testy food today");
+print_r($b);
+echo "<br>";*/
+
+?>
