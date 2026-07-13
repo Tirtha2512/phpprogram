de9b5c033bf6b412eb1139b3f3eb321f2986849b
Tirtha2512 <tirthagohel159@gmail.com>
2026-07-13 11:40:47 +0530

    Add files via upload

--- string.php
+++ string.php
@@ -23,4 +23,41 @@ echo "<br>";
 print_r($b);
 echo "<br>";*/
 
+
+echo strlen("this is my first program");
+echo "<br>";
+
+
+
+$a=trim(" this ");
+echo $a;
+echo "<br>";
+$b=strlen($a);
+echo $b;
+echo "<br>";
+
+
+$c=substr("Tirtha",2);
+echo $c;
+echo "<br>";
+
+ echo substr_count("tirtha","t");
+ echo "<br>";
+
+
+ echo substr_compare("i like", "  like", 3);
+ echo "<br>";
+
+echo strcmp("gohel", "go");
+ echo "<br>";
+
+
+echo strstr(" i am tirtha","a");
+echo "<br>";
+
+echo strrev("i love php");
+echo "<br>";
+
+echo str_replace("love", "love", "like");
+echo "<br>";
 ?>
