fd81e7f72e1c09cbb7ae67f2fb48a2524a1e4af7
Tirtha2512 <tirthagohel159@gmail.com>
2026-07-23 10:49:13 +0530

    Add files via upload

---
+++ Assignment_2.php
@@ -0,0 +1,17 @@
+1:Simple Function
+<?php
+Function arithmetic()
+{
+		$a=25;
+		$b=5;
+		echo "Addition:",$a+$b;
+		echo "<br>";
+		echo "Subtrection:",$a-$b;
+		echo "<br>";
+		echo "Multiplication:",$a*$b;
+		echo "<br>";
+		echo "Division:",$a/$b;
+
+}
+arithmetic();
+?>
