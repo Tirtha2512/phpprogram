dc6ba2472c6d97e4c86d58c25d6e7e0e09203f6d
Tirtha2512 <tirthagohel159@gmail.com>
2026-07-23 10:49:46 +0530

    Add files via upload

---
+++ Assignment_2.3.php
@@ -0,0 +1,13 @@
+<?php
+//1:Indexed array
+$a=array(10,11,12,13,14);
+echo $a[2];
+echo "<br>";
+?>
+
+
+<?php
+//2:Associative array
+$b=array("Zero"=>0,"One"=>1,"Two"=>2,"Three"=>3,"Zero"=>0,"Four"=>4);
+echo $b['Three'];
+?>
