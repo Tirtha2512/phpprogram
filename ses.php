470df738aeb17d99ef3d830a5ccb9e08dbfa5509
Tirtha2512 <tirthagohel159@gmail.com>
2026-08-03 11:20:18 +0530

    Add files via upload

---
+++ ses.php
@@ -0,0 +1,5 @@
+<form action="session.php" method="POST">
+	username<input type="text" name="username"><br>
+	password<input type="number" name="pwd"><br>
+	<input type="submit" name="submit" value="login">
+</form>	
