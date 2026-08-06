82f7db03222b5265e3d0440eb35141dd832660b5
Tirtha2512 <tirthagohel159@gmail.com>
2026-08-06 10:11:57 +0530

    Add files via upload

---
+++ arrayform.php
@@ -0,0 +1,6 @@
+<form action="arraycookie.php" method="POSt">
+		name:<input type="text" name="name">
+		university:<input type="text" name="university">
+		city:<input type="text" name="city">
+		<input type="submit" name="submit">
+	</form>
