66c9fe0065636d8d82961e8e8cd0d68a1af049dc
Tirtha2512 <tirthagohel159@gmail.com>
2026-09-24 10:44:01 +0530

    Add files via upload

--- display.php
+++ display.php
@@ -8,14 +8,35 @@
 		<th>delete</th>
 		<th>update</th>
 	</tr>
-</table>
+
 <?php
-inclued("connection.php");
+$con=mysqli_connect("localhost","root","","university");
+if(!$con)
+{
+	die("connection is not done");
+}
 $qry="select * from students";
 $result=mysqli_query($con,$qry);
 if(mysqli_num_rows($result)>0)
 {
-	
-}
+	while($row=mysqli_fetch_assoc($result))
+	{
+		echo "<tr>";
+		echo "<td>".$row['id']."</td>";
+		echo "<td>".$row['name']."</td>";
+		echo "<td>".$row['dept']."</td>";
+		echo "<td>".$row['mob']."</td>";
+		echo "<td>".$row['dob']."</td>";
+		echo "<td>";
+			echo "<a href='delete.php?id=".$row['id']."'>delete</a>";
+		echo "</td>";
 
+		echo "<td>";
+			echo "<a href='update.php?id=".$row['id']."'>update</a>";
+		echo "</td>";
+		echo "</tr>";
+
+	}
+}
+    echo "</table>"
 ?>
